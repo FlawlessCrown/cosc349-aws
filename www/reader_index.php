a4aabb2cdfa0cbbfb5342d630f2efb1e62178a43
SilSnowball <silsnowball@gmail.com>
2019-10-02 02:35:18 +1300

    Removed and Changed Reader

--- www/reader_index.php
+++ www/reader_index.php
@@ -38,7 +38,7 @@
 				margin-right:auto;
 			}
 			button {
-				width: 170px;
+				width: 160px;
 			}
 		</style>
 	</head>
@@ -46,15 +46,35 @@
 		<section>
 			<h1>Note Reader Page</h1>
 			<form action="db/deleteall.php" method="post">
-				<button type="submit" value="Delete All Notes" name="deleteall">Delete All Notes From:</button>
+				<button type="submit" value="Delete All Notes" name="deleteall">Delete User Notes:</button>
 				<input type="text" name="userid" id="userid" maxlength="9">
 			</form>
-			<form action="searchuser.php" method="post">
+			<form action="reader_index.php" method="post">
 				<button type="submit" value="View User" name="viewuser">View User:</button>
 				<input type="text" name="userid" id="userid" maxlength="9" value="default">
 			</form>
 			<table border="1">
-				<?php require 'db/read.php';?>
+				<?php
+				$dbhost = 'webnotes.cn4fbzuyvxpk.us-east-1.rds.amazonaws.com';
+				$dbname = 'notes';
+				$dsn = "mysql:host={$dbhost};dbname={$dbname};";
+				$username = 'admin';
+				$password = '!4h5CDEdyph8';
+				if(isset($_POST[userid])){
+					$userid = $_POST[userid];
+				} else {
+					$userid = 'default';
+				}
+				$pdo = new PDO($dsn, $username, $password);
+				if($userid=='showall') {
+					$q = $pdo->query("SELECT * FROM webNotes ORDER BY userID ASC;");
+				} else {
+					$q = $pdo->query("SELECT * FROM webNotes WHERE userID='$userid' ORDER BY userID ASC;");
+				}
+				while($row 	= $q->fetch() or die(mysql_error())){
+				  echo "<tr><td>".$row["note"]."</td><td>".$row["userID"]."</td></tr>\n";
+				}
+				?>
 			</table>
 		</section>
 	</body>
